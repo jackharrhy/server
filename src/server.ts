3d078a16c4dc914d8ca7627307564bbe4464e6ff
Jack Arthur Harrhy <me@jackharrhy.com>
2022-10-10 22:41:26 -0230

    c o r s

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import { logger } from "hono/logger";
+import { cors } from 'hono/cors';
 
 import { now } from "./routes/now";
 
@@ -8,6 +9,7 @@ const app = new Hono();
 
 app.use("*", logger());
 app.use("*", prettyJSON());
+app.use("*", cors());
 
 app.route("/now", now);
 
